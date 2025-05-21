30ee5c35d028edddf97522c7d3dab9301ef8fb80
dan-class <danielns.py@gmail.com>
2025-05-21 20:10:00 -0300

    project created

---
+++ src/main/java/com/danielnoronha/dnlist/DnlistApplication.java
@@ -0,0 +1,13 @@
+package com.danielnoronha.dnlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielnoronha/dnlist/DnlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielnoronha.dnlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
